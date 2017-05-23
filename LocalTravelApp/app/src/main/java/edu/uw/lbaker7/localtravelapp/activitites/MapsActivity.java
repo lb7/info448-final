342c3d9e3002ebe861de8fa21b4116cd16850d5b
Christa Joy Jaeger <cjjaeger@uw.edu>
2017-05-23 16:20:49 -0700

    connection not working

--- LocalTravelApp/app/src/main/java/edu/uw/lbaker7/localtravelapp/activitites/MapsActivity.java
+++ LocalTravelApp/app/src/main/java/edu/uw/lbaker7/localtravelapp/activitites/MapsActivity.java
@@ -1,8 +1,21 @@
 package edu.uw.lbaker7.localtravelapp.activitites;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,9 +25,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import edu.uw.lbaker7.localtravelapp.R;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, OnConnectionFailedListener {
+    private static final int LOCATION_REQUEST_CODE = 1;
+    private PlaceLikelihoodBuffer places;
+    private static final String TAG = "MapsActivity";
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +42,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
     }
 
 
@@ -44,5 +68,80 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+//
+//
+//    @Override
+//    public void onLocationChanged(Location location) {
+//       // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=
+//    }
+@Override
+protected void onStart() {
+    Log.v(TAG, "Started!!!");
+
+    mGoogleApiClient.connect();
+    super.onStart();
+}
+
+    @Override
+    protected void onStop() {
+        Log.v(TAG, "Stoped!!!");
+
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.v(TAG, "onConnectionFailed");
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.v(TAG, "onConnection Here!!!");
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            //have permission, can go ahead and do stuff
+            Log.v(TAG, "got permission");
+           Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null).setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+               @Override
+               public void onResult(@NonNull PlaceLikelihoodBuffer placeLikelihoods) {
+                   places = placeLikelihoods;
+                   Log.v(TAG, placeLikelihoods.toString());
+               }
+           });
+            //PlaceDetectionApi
+            //assumes location settings enabled
+            //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, , this);
+
+        }
+        else {
+            //request permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch(requestCode){
+            case LOCATION_REQUEST_CODE: { //if asked for location
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    onConnected(null); //do whatever we'd do when first connecting (try again)
+                }
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.v(TAG, "onConnectionSuspended");
+
+
     }
 }
