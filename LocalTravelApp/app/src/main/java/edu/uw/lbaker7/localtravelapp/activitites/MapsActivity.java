1906205c325c15f70737752c87c8acfba7f4437d
Kar Yin Ng <karyin3@uw.edu>
2017-05-31 22:18:58 -0700

    working on colors and layout

--- LocalTravelApp/app/src/main/java/edu/uw/lbaker7/localtravelapp/activitites/MapsActivity.java
+++ LocalTravelApp/app/src/main/java/edu/uw/lbaker7/localtravelapp/activitites/MapsActivity.java
@@ -157,11 +157,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        /*LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-
         if(stuff.size() >0){
             String waypoint = "";
 
@@ -447,7 +442,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 ft.replace(R.id.map, placeListFragment);
                 ft.addToBackStack(null);
                 ft.commit();
-                item.setVisible(false);
                 controls.setVisibility(View.INVISIBLE);
                 return true; //handled
             case R.id.settings:
